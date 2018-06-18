45008e751b7ab0b8e328f0c0e08d74dbe51378ae
たしまとおる <tashima.toru@gmail.com>
2018-06-19 01:57:30 +0900

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -1,3 +1,12 @@
+# ssh(private key)
+
+cd ~/
+mkdir .ssh
+chmod 700 .ssh
+touch ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+
 # Docker
 
 curl -sSL https://get.docker.com | sh
@@ -5,11 +14,31 @@ sudo usermod -aG docker `whoami`
 exec $SHELL -l
 docker --version
 
+
+
 # Docker-compose
 
+cd ~/
 git clone https://github.com/docker/compose.git
 cd compose
 sudo docker build -t docker-compose:armhf -f Dockerfile.armhf .
 sudo docker run --rm --entrypoint="script/build/linux-entrypoint" -v $(pwd)/dist:/code/dist -v $(pwd)/.git:/code/.git "docker-compose:armhf"
 sudo cp dist/docker-compose-Linux-armv7l /usr/local/bin/docker-compose
 docker-compose --version
+## remove
+cd ~/
+rm -rf compose/
+
+
+
+# update Raspbian
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y dist-upgrade
+
+
+
+# reboot
+
+sudo reboot
